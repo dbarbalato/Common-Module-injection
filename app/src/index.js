ff7d88f552f9e5c1d95ff5669c08398d882b4abb
Dave Barbalato <dbarbalato@gmail.com>
2018-08-29 23:33:34 -0400

    Better example

--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,3 @@
-const sayHello = require('core/sample/hi');
+const hi = require('core/sample/hi');
 
-console.log(sayHello.default('Dave'));
+console.log(hi.default('Dave'));
--- core/src/sample/hi.js
+++ core/src/sample/hi.js
@@ -1,3 +1,3 @@
-const sayHello = name => `Hey, ${name}!`;
+const sayHello = name => `Hi, ${name}!`;
 
 export default sayHello;
